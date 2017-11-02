3e1ae825001d0cbb096143d205f477d64e4ae482
そうですか <anatagadaisukidesu@hotmail.com>
2017-11-02 23:36:01 +0800

    上传视频或音频时，超时设为5分钟

--- app/src/main/java/com/gzlk/android/isp/api/upload/UploadRequest.java
+++ app/src/main/java/com/gzlk/android/isp/api/upload/UploadRequest.java
@@ -121,6 +121,7 @@ public class UploadRequest extends Request<Upload> {
      * 上传单个文件
      */
     public void upload(final String file) {
+        liteHttp.getConfig().setConnectTimeout(300000).setSocketTimeout(300000);
         liteHttp.executeAsync(request(file));
     }
 }
