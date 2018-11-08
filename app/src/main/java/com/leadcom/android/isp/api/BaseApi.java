15e4edd09d8aebe9a34784a1c6e38513f479520e
HsiangLeekwok <xiang.l.g@gmail.com>
2018-11-08 16:32:26 +0800

    api地址通过gradle控制

--- app/src/main/java/com/leadcom/android/isp/api/BaseApi.java
+++ app/src/main/java/com/leadcom/android/isp/api/BaseApi.java
@@ -22,7 +22,7 @@ abstract class BaseApi implements Serializable {
     /**
      * 所有网络调用的网址前缀
      */
-    static final String URL = Cache.isReleasable() ? "https://www.py17w.com" : "http://113.108.144.2:8889";
+    static final String URL = StringHelper.getString(R.string.app_api_http_host);
 
     static final String API_VER = StringHelper.getString(Cache.isReleasable() ? R.string.app_api_version_release : R.string.app_api_version);
     /**
